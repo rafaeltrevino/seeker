e62573e3c2f7c44f716a2afa8f5a17f599c96052
trevino <rafael.trevino@me.com>
2019-01-20 16:38:49 -0500

    Added JawsDB to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,17 @@
 const mysql = require("mysql");
+var connection;
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "seeker_db"
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "seeker_db"
+}
+
+const 
 });
 
 connection.connect(function(err) {
